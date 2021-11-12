f921c0add300b26df2942fce1c4b794a92a03659
amirzulic <nokia5130>
2021-11-12 18:28:31 +0100

    Fixed login password limitation

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -20,6 +20,7 @@ function Login() {
             email: Yup.string().email('Invalid email address').required('Required'),
             password: Yup.string()
                 .min(8, 'Must be 8 characters or more')
+                .max(15, 'Must be 15 characters or less')
                 .required('Required')
         }),
         onSubmit: values => {
