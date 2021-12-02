0562b8700cd7d7f181055366bd203eb3f8266f9a
amirzulic <nokia5130>
2021-12-02 17:07:06 +0100

    Deleted base url bugs

--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "https://backend-app-auction.herokuapp.com/";
+const BASE_URL = "https://backend-app-auction.herokuapp.com";
 
 export const loadLandingPageProducts = async () => {
     return await axios.get(BASE_URL + "/products");
