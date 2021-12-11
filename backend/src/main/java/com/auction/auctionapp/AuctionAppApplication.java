4a8948f4a0d3cc1aba90da705dfaead050527ccf
amirzulic <nokia5130>
2021-12-12 00:50:10 +0100

    Final main changes

--- backend/src/main/java/com/auction/auctionapp/AuctionAppApplication.java
+++ backend/src/main/java/com/auction/auctionapp/AuctionAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 import java.security.Security;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
