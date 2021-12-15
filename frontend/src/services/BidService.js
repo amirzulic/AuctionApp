84b430a406645640bf9a7a9fd5fb96723ee49c0a
amirzulic <nokia5130>
2021-12-15 17:28:22 +0100

    Added heroku env variables to frontend services

--- frontend/src/services/BidService.js
+++ frontend/src/services/BidService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const saveBid = async (bid, header) => {
-    return await axios.post( process.env.REACT_APP_BASE_URL + "/bid", bid, {headers: {"Authorization": header}})
+    return await axios.post( process.env.REACT_APP_D_URL + "/bid", bid, {headers: {"Authorization": header}})
 }
 
 export const loadBid = async(id) => {
-    return await axios.get(process.env.REACT_APP_BASE_URL + "/bid-info", {params: {productId: id}});
+    return await axios.get(process.env.REACT_APP_D_URL + "/bid-info", {params: {productId: id}});
 }
--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -13,30 +13,30 @@ export const loadNewArrivals = async () => {
 }
 
 export const loadLastChance = async () => {
-    return await axios.get(process.env.REACT_APP_BASE_URL + "/products/last");
+    return await axios.get(process.env.REACT_APP_D_URL + "/products/last");
 }
 
 export const loadDefaultSortingByCategory = async (id) => {
-    return await axios.get(process.env.REACT_APP_BASE_URL + "/products/default-sorting", {params: {productCategoryId: id}});
+    return await axios.get(process.env.REACT_APP_D_URL + "/products/default-sorting", {params: {productCategoryId: id}});
 }
 
 export const loadNewArrivalsByCategory = async (id) => {
-    return await axios.get(process.env.REACT_APP_BASE_URL + "/products/new-category", {params: {productCategoryId: id}});
+    return await axios.get(process.env.REACT_APP_D_URL + "/products/new-category", {params: {productCategoryId: id}});
 }
 
 export const loadLastChanceByCategory = async (id) => {
-    return await axios.get(process.env.REACT_APP_BASE_URL + "/products/last-category", {params: {productCategoryId: id}});
+    return await axios.get(process.env.REACT_APP_D_URL + "/products/last-category", {params: {productCategoryId: id}});
 }
 
 export const loadHighToLowByCategory = async (id) => {
-    return await axios.get(process.env.REACT_APP_BASE_URL + "/products/high-low", {params: {productCategoryId: id}});
+    return await axios.get(process.env.REACT_APP_D_URL + "/products/high-low", {params: {productCategoryId: id}});
 }
 
 export const loadLowToHighByCategory = async (id) => {
-    return await axios.get(process.env.REACT_APP_BASE_URL + "/products/low-high", {params: {productCategoryId: id}});
+    return await axios.get(process.env.REACT_APP_D_URL + "/products/low-high", {params: {productCategoryId: id}});
 }
 
 export const loadProduct = async (id, header) => {
-    return await axios.get(process.env.REACT_APP_BASE_URL + "/product", {params: {productId: id}});
+    return await axios.get(process.env.REACT_APP_D_URL + "/product", {params: {productId: id}});
 }
 
